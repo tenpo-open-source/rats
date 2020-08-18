c2ffe43a8fad94e3e063c36ea755fe35436399c8
Jose Miguel Lueiza <jose.lueiza@tenpo.cl>
2020-08-18 12:42:20 -0400

    arreglo en test

--- tests/getData.js
+++ tests/getData.js
@@ -4,27 +4,36 @@ const proxyquire = require('proxyquire');
 const { expect } = chai;
 
 describe('getData', () => {
-  it('should write a file with results', (done) => {
-    let getData = proxyquire('../getData.js', {
-        'mongoose': {
-            connect: () => {return;},
-            model:() => {
-                return {
-                    find: () => {return Promise.resolve([{timestamp:"343434", serviceTime:"324324"}])}
+    let getData;
+    let file;
+    let line;
+    before((done) => {
+        getData = proxyquire('../getData.js', {
+            'mongoose': {
+                connect: () => {return;},
+                model:() => {
+                    return {
+                        find: () => {return Promise.resolve([{timestamp:"343434", serviceTime:"324324"}])}
+                    }
+                }
+            },
+            'fs': {
+                writeFileSync: (fileTmp) => {
+                    file = fileTmp
+                },
+                appendFileSync: (fileTmp, lineTmp) => {
+                    file = fileTmp
+                    line = lineTmp
+                    done()
                 }
             }
-        },
-        'fs': {
-            writeFileSync: (file) => {
-                expect(file).to.equal('results.csv')
-            },
-            appendFileSync: (file, line) => {
-                expect(file).to.equal('results.csv');
-                expect(line).to.equal('343434;324324\n')
-                done();
-            }
-        }
+        });
+    })
+    it('should write a file with results', (done) => {
+        expect(file).to.equal('results.csv')
+        expect(file).to.equal('results.csv');
+        expect(line).to.equal('343434;324324\n')
+        done();
     });
-  });
 
 });
