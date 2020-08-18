c8b4f1c94ddca9b4d3546c3a297a9f07a883712d
Jose Miguel Lueiza <jose.lueiza@tenpo.cl>
2020-08-18 11:38:26 -0400

    se agregan tests a getData.js

---
+++ tests/getData.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const proxyquire = require('proxyquire');
+
+const { expect } = chai;
+
+describe('getData', () => {
+  it('should write a file with results', (done) => {
+    let getData = proxyquire('../getData.js', {
+        'mongoose': {
+            connect: () => {return;},
+            model:() => {
+                return {
+                    find: () => {return [{timestamp:"343434", serviceTime:"324324"}]}
+                }
+            }
+        },
+        'fs': {
+            writeFileSync: (file) => {
+                expect(file).to.equal('results.csv')
+                done();
+            },
+            appendFileSync: (file, line) => {
+                expect(file).to.equal('results.csv');
+                expect(line).to.equal('343434;324324\n')
+            }
+        }
+    });
+  });
+
+});
