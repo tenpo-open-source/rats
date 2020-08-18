b15184647c926bffec8ae5ce1fe6096e54adcfa3
Jose Miguel Lueiza <jose.lueiza@tenpo.cl>
2020-08-18 14:20:03 -0400

    mejora en el reporte para codecov

--- tests/getData.js
+++ tests/getData.js
@@ -21,7 +21,8 @@ describe('getData', () => {
                 writeFileSync: (fileTmp) => {
                     file = fileTmp
                     done()
-                }
+                },
+                appendFileSync:()=>{return}
             }
         });
     })
